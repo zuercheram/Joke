5a0d4a436d53af9f5c90abd8d2039ca7b562a814
Amos Zürcher <amos.zuercher@gmail.com>
2025-05-21 14:08:45 +0200

    updat

--- src/Joke.Client/Program.cs
+++ src/Joke.Client/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
